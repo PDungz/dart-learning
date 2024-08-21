873b3e4e805cba706d4d80810bce5d33631754f4
LAPTOP-NKUME37F\ASUS <phungvandung030623@gmail.com>
2024-08-21 22:33:46 +0700

    update master

--- bin/dart_learn.dart
+++ bin/dart_learn.dart
@@ -2,4 +2,5 @@ import 'package:dart_learn/dart_learn.dart' as dart_learn;
 
 void main(List<String> arguments) {
   print('Hello world: ${dart_learn.calculate()}!');
+  print('Hello world: ${dart_learn.calculate()}!');
 }
